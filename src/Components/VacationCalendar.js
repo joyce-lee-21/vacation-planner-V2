4a4015dfc8730bef1cde7e58f56c168b9d490579
smsguy927 <smsguy927@gmail.com>
2021-06-23 17:25:36 -0500

    Fixed some calendar issues but there is still bad wrap around

--- src/Components/VacationCalendar.js
+++ src/Components/VacationCalendar.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import { addDays } from "date-fns";
+import { first } from "lodash";
 
-  // API Key from https://rapidapi.com/community/api/open-weather-map/
-  const API_KEY = '';
+// API Key from https://rapidapi.com/community/api/open-weather-map/
+const API_KEY = "0629feec2bmsh3ef7f3d86a812b3p127915jsna97cfce97a10";
+const API_HOST = "community-open-weather-map.p.rapidapi.com";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,24 +38,61 @@ const useStyles = makeStyles((theme) => ({
 }));
 // !! This function will replace the icon placeholders below in the FormRow component
 
+const SECONDS_PER_DAY = 86400;
+const MONTHS = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December"
+};
+
 function VacationCalendar() {
   const classes = useStyles();
 
   const [forecastArray, setForecastArray] = useState([]);
 
+  const [currentMonth, setCurrentMonth] = useState("");
+
+  function leftPadWithEmptyObject(arr, seconds) {
+    arr.unshift({ dt: seconds });
+  }
+
+  function buildForecastArray(forecast) {
+    let firstDayOfWeek = new Date(forecast[0].dt * 1000).getDay();
+    setCurrentMonth(new Date(forecast[0].dt * 1000).getMonth());
+    let secondsInFirstDay = forecast[0].dt;
+    console.log(firstDayOfWeek);
+    while (firstDayOfWeek > 0) {
+      secondsInFirstDay -= SECONDS_PER_DAY;
+      leftPadWithEmptyObject(forecast, secondsInFirstDay);
+      firstDayOfWeek--;
+    }
+
+    console.log(forecast);
+    setForecastArray(forecast);
+  }
+
   useEffect(() => {
     fetch(
-      "https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=honolulu&cnt=7&units=imperial&mode=JSON",
+      "https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=honolulu&cnt=10&units=imperial&mode=JSON",
       {
         method: "GET",
         headers: {
           "x-rapidapi-key": API_KEY,
-          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+          "x-rapidapi-host": API_HOST
         }
       }
     )
       .then((response) => response.json())
-      .then((data) => setForecastArray(data.list));
+      .then((data) => buildForecastArray(data.list));
   }, []);
 
   // const weatherIcon = () => {
@@ -81,25 +121,39 @@ function VacationCalendar() {
       <React.Fragment>
         <Grid item xs={2}></Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Sun</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader0">
+            Sun
+          </Paper>
         </Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Mon</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader1">
+            Mon
+          </Paper>
         </Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Tues</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader2">
+            Tues
+          </Paper>
         </Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Wed</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader3">
+            Wed
+          </Paper>
         </Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Thurs</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader4">
+            Thurs
+          </Paper>
         </Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Fri</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader5">
+            Fri
+          </Paper>
         </Grid>
         <Grid item xs={1}>
-          <Paper className={classes.paper}>Sat</Paper>
+          <Paper className={classes.paper} id="dayWeekHeader6">
+            Sat
+          </Paper>
         </Grid>
         <Grid item xs={1}></Grid>
       </React.Fragment>
@@ -112,22 +166,26 @@ function VacationCalendar() {
         <React.Fragment>
           <Grid item xs={2}></Grid>
           {forecastArray.map((daily) => {
-            const conditions = daily.weather[0].icon;
+            const conditions = daily.weather && daily.weather[0].icon;
             return (
-              <Grid item xs={1} key={daily.dt}>
+              <Grid item xs={1} key={daily?.dt}>
                 <Paper className={classes.date}>
-                  {new Date(daily.dt * 1000).getDate()}
+                  {new Date(daily?.dt * 1000).getDate()}
                 </Paper>
                 <Paper className={classes.weather}>
                   <div className="weather-elements">
                     <span className="temp-number">
-                      {Math.round(daily.temp.day)}°
+                      {daily.temp
+                        ? `${Math.round(daily.temp.day)}°`
+                        : "no data"}
                     </span>
-                    <img
-                      src={`http://openweathermap.org/img/wn/${conditions}@2x.png`}
-                      alt="weather-icon"
-                      className="weather-icon"
-                    />
+                    {conditions && (
+                      <img
+                        src={`http://openweathermap.org/img/wn/${conditions}@2x.png`}
+                        alt="weather-icon"
+                        className="weather-icon"
+                      />
+                    )}
                   </div>
                 </Paper>
               </Grid>
@@ -141,6 +199,7 @@ function VacationCalendar() {
   return (
     <div className="calendar-container">
       <h2>Vacation Calendar</h2>
+      <h3>{MONTHS[currentMonth]}</h3>
       <div className={classes.root}>
         <Grid container spacing={1}>
           <Grid container item xs={12} spacing={0}>
